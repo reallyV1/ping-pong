dcc37fafb7b727b78677b226baf61c82fc851d43
reallyV1 <130216749+reallyV1@users.noreply.github.com>
2023-04-15 16:57:33 +0700

    Add files via upload
    1 and 2 player

---
+++ ping_pong.py
@@ -0,0 +1,57 @@
+from pygame import *
+clock = time.Clock()
+FPS = 60
+class GameSprite(sprite.Sprite):
+    def __init__(self,image_file,plaer_x,plaer_y,width,height,plaer_speed):
+        super().__init__()
+        #self.image=transform.scale(image.load(image_file),(width,height))
+        self.image=Surface((width,height))
+        self.image.fill((255,255,255))
+        self.speed=plaer_speed
+        self.rect=self.image.get_rect()
+        self.rect.x=plaer_x
+        self.rect.y=plaer_y
+        self.direction = 'left'
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+class Plaer1 (GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        #управление тут
+        if keys[K_w] and self.rect.y>0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y<400:
+            self.rect.y += self.speed
+class Plaer2 (GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        #управление тут
+        if keys[K_UP] and self.rect.y>0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y<400:
+            self.rect.y += self.speed
+beckground_color = (0,0,255)
+beckground = Surface((700,500))
+beckground.fill(beckground_color)
+window = display.set_mode((700,500))
+player1 = Plaer1('rocket.png',100,400,25,100,10)
+player2 = Plaer2('rocket.png',500,400,25,100,10)
+game = True 
+finish = False
+font.init()
+font2 = font.SysFont('Arial', 30)
+win=font2.render('Ты выйграл', True, (0, 255, 0))
+lose=font2.render('Ты проиграл', True, (255, 0, 0))
+while game == True:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish == False:
+        window.blit(beckground,(0,0))
+        player1.reset()
+        player1.update()
+        player2.reset()
+        player2.update()
+        display.update()
+        clock.tick(FPS)
+        
